55481b6263bd60aec71c4c40dced0fe8d07a5434
TinaTien <tinajay8427@gmail.com>
2015-12-01 23:50:16 +0800

    new choose photo from album

--- Recipe/AddRecipeViewController.swift
+++ Recipe/AddRecipeViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class AddRecipeViewController: UIViewController {
+class AddRecipeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,9 +16,44 @@ class AddRecipeViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    // 選取相冊
+    @IBAction func fromAlbum(sender: AnyObject ) {
+        //判斷設置是否支持圖片庫
+        
+        if UIImagePickerController .isSourceTypeAvailable(. PhotoLibrary ){
+            //初始化圖片控制器
+            let picker = UIImagePickerController ()
+            //設置代理
+            picker.delegate = self
+            //指定圖片控制器類型
+            picker.sourceType = UIImagePickerControllerSourceType . PhotoLibrary
+            //設置是否允許編輯
+            self .presentViewController(picker, animated: true , completion: {
+                () -> Void in
+            })
+        } else {
+            print ( "讀取相冊錯誤" )
+        }
+        
+    }
+    
+    //選擇圖片成功後代理
+    func imagePickerController(picker: UIImagePickerController ,
+        didFinishPickingMediaWithInfo info: [ String : AnyObject ]) {
+            
+            //查看info對象
+            print (info)
+            //獲取選擇的原圖
+            let image = info[ UIImagePickerControllerOriginalImage ] as! UIImage
+            imageView.image = image
+            
+            //圖片控制器退出
+            picker.dismissViewControllerAnimated( true , completion: {
+                () -> Void in
+            })
     }
     
     var types = ["點心烘焙","米食","麵食","湯品","副餐"]
@@ -36,4 +71,10 @@ class AddRecipeViewController: UIViewController {
         return types[row]
         
     }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
 }
--- Recipe/StepsViewController.swift
+++ Recipe/StepsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class StepsViewController: UIViewController, UIScrollViewDelegate {
+class StepsViewController: UIViewController, UIScrollViewDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var stepsPageControl: UIPageControl!
     
@@ -42,7 +42,7 @@ class StepsViewController: UIViewController, UIScrollViewDelegate {
         let view2 = UIView(frame: CGRectMake(pageWidth, 0, pageWidth, pageHeight))
         
         view2.backgroundColor = UIColor(red: 241/255, green: 196/255, blue: 15/255, alpha: 1)
-   
+        
         let label2  = UILabel(frame: CGRect(x: 28,y: 0,width: pageWidth/4,height: pageHeight/5))
         
         label2.text = " Step 2"
@@ -57,7 +57,7 @@ class StepsViewController: UIViewController, UIScrollViewDelegate {
         let view3 = UIView(frame: CGRectMake(2*pageWidth, 0, pageWidth, pageHeight))
         
         view3.backgroundColor = UIColor(red: 26/255, green: 188/255, blue: 156/255, alpha: 1)
-     
+        
         
         let label3  = UILabel(frame: CGRect(x: 28,y: 0,width: pageWidth/4,height: pageHeight/5))
         
@@ -70,7 +70,6 @@ class StepsViewController: UIViewController, UIScrollViewDelegate {
         description3.backgroundColor = UIColor.whiteColor()
         
         
-        
         scrollView.addSubview(view1)
         scrollView.addSubview(view2)
         scrollView.addSubview(view3)
@@ -81,12 +80,61 @@ class StepsViewController: UIViewController, UIScrollViewDelegate {
         view2.addSubview(description2)
         view3.addSubview(description3)
         
+        // 新增選擇相冊按鈕
+        let button   = UIButton(type: UIButtonType.System) as UIButton
+        button.frame = CGRectMake(28, 260, pageWidth/2, pageHeight/11)
+        button.backgroundColor = UIColor.grayColor()
+        button.setTitle("choose photo", forState: UIControlState.Normal)
+        button.addTarget(self, action: "fromAlbum:", forControlEvents: UIControlEvents.TouchUpInside)
+        view1.addSubview(button)
     }
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    // 選取相冊
+    @IBAction func fromAlbum(sender: AnyObject ) {
+        //判斷設置是否支持圖片庫
+        
+        if UIImagePickerController .isSourceTypeAvailable(. PhotoLibrary ){
+            //初始化圖片控制器
+            let picker = UIImagePickerController ()
+            //設置代理
+            picker.delegate = self
+            //指定圖片控制器類型
+            picker.sourceType = UIImagePickerControllerSourceType . PhotoLibrary
+            //設置是否允許編輯
+            self .presentViewController(picker, animated: true , completion: {
+                () -> Void in
+            })
+        } else {
+            print ( "讀取相冊錯誤" )
+        }
+        
+    }
+    
+    //選擇圖片成功後代理
+    func imagePickerController(picker: UIImagePickerController ,
+        didFinishPickingMediaWithInfo info: [ String : AnyObject ]) {
+            
+            //查看info對象
+            print (info)
+            //獲取選擇的原圖
+            let image = info[ UIImagePickerControllerOriginalImage ] as! UIImage
+            imageView.image = image
+            
+            //圖片控制器退出
+            picker.dismissViewControllerAnimated( true , completion: {
+                () -> Void in
+            })
+    }
+    
     
     @IBAction func pageControlDidPage(sender: AnyObject) {
         let xOffset = scrollView.bounds.width * CGFloat(stepsPageControl.currentPage)
         scrollView.setContentOffset(CGPointMake(xOffset,0) , animated: true)
     }
+    
+    
     
     func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
         stepsPageControl.currentPage = Int(scrollView.contentOffset.x / scrollView.bounds.width)
